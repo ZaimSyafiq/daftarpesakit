6763fde475e67810dbc14a123ed04f94ff0d2d77
ZaimSyafiq <60953061+ZaimSyafiq@users.noreply.github.com>
2020-02-12 12:58:18 +0800

    Updatedconnection.php

--- connection.php
+++ connection.php
@@ -1,4 +1,4 @@
 <?php
-$conn = mysqli_connect('127.0.0.1','root','');
-mysqli_select_db($conn,'penawarbarakah');
+$conn = mysqli_connect('tj5iv8piornf713y.cbetxkdyhwsb.us-east-1.rds.amazonaws.com','aqmljqsu6fto79sb','edpzs8pvzxz9nh2e');
+mysqli_select_db($conn,'c4luno1bkgzed0ou');
 ?>
